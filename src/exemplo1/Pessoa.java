0832a91cb9cbbd5ac47cfffe820abc6addeb753d
ANTONIO AUGUSTO PENHA DO NASCIMENTO <a935910@rn.senac.br>
2018-07-23 21:10:16 -0300

    Aula Java Objetos

---
+++ src/exemplo1/Pessoa.java
@@ -0,0 +1,61 @@
+package exemplo1;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	private float altura;
+	private float peso;
+		
+	
+
+	public Pessoa(String nome, int idade, float altura, float peso) {
+		super();
+		this.setNome(nome);
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+	}
+	
+	public Pessoa(String nome) {
+		super();
+		this.setNome(nome);
+	}
+	
+	public Pessoa() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public float getAltura() {
+		return altura;
+	}
+	
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	
+	public float getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	
+}
---
+++ src/exemplo1/TestaPessoa.java
@@ -0,0 +1,12 @@
+package exemplo1;
+
+public class TestaPessoa {
+
+	public static void main(String[] args) {
+		Pessoa pessoa = new Pessoa("Leandro", 25, 1.75f, 72f);
+			Pessoa leandro = new Pessoa(null, 0, 0, 0);
+			Pessoa sobrinho= new Pessoa();
+			Pessoa ademir = new Pessoa("Ademir");
+	}
+
+}
